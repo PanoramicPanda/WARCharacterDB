3e914df32ba354aa981d5946ee35290395b7dbfa
PanoramicPanda <uriah.h.brown@gmail.com>
2018-04-21 17:11:00 -0400

    Slate Default + Restructure of NavBar Script

--- scripts/base_html_generator.php
+++ scripts/base_html_generator.php
@@ -1,7 +1,8 @@
 <?php
 
 function generate_html_head($title) {
-    $bootstrap_cdn = $_SESSION['bootstrap_cdn'] ?? 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css';
+    $bootstrap_cdn = $_SESSION['bootstrap_cdn'] ?? 'https://stackpath.bootstrapcdn.com/bootswatch/4.1.0/slate/bootstrap.min.css';
+//    $bootstrap_cdn = $_SESSION['bootstrap_cdn'] ?? 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css';
     echo '<!doctype html>
     <html lang="en">
         <head>
@@ -22,7 +23,7 @@ function generate_html_tail(){
         <!-- jQuery first, then Popper.js, then Bootstrap JS -->
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
         <script defer src="https://use.fontawesome.com/releases/v5.0.10/js/all.js" integrity="sha384-slN8GvtUJGnv6ca26v8EzVaR9DC58QEwsIk9q1QXdCU8Yu8ck/tL/5szYlBbqmS+" crossorigin="anonymous"></script>
     </body>
 </html>';
