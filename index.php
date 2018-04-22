beca352524cf6bef6748a3cd805c42893db6c6d2
PanoramicPanda <uriah.h.brown@gmail.com>
2018-04-22 15:11:19 -0400

    Adding Card element to index - no real content

--- index.php
+++ index.php
@@ -4,5 +4,14 @@
     require 'scripts/basic_html_elements.php';
 
 generate_html_head('WAR Character Database');
-generate_bootstrap_navbar();
+generate_bootstrap_navbar(); ?>
+<div class="card mx-2">
+    <div class="card-header">
+        Test
+    </div>
+    <div class="card-body"
+         Derp
+    </div>
+</div>
+<?php
 generate_html_tail();
