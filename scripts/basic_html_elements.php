95c851df926fdb6f8a1d46431a63b91fc5f93c7f
PanoramicPanda <uriah.h.brown@gmail.com>
2018-04-22 15:32:43 -0400

    Tweaking Nav

--- scripts/basic_html_elements.php
+++ scripts/basic_html_elements.php
@@ -5,15 +5,15 @@ function generate_bootstrap_navbar(){
 
     #======= Nav Element Construction =======
 
-    $navbar = '<nav class="navbar navbar-dark navbar-expand-lg bg-dark my-2 py-2">
+    $navbar = '<nav class="navbar navbar-expand-lg my-2">
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+                        <i class="fas fa-bars fa-2x"></i>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">';
 
     #======= Left Side Nav =======
 
-    $navbar .= '<ul class="navbar-nav mr-auto">';
+    $navbar .= '<ul class="navbar-nav mr-auto"><li class="nav-item"><a class="nav-link" href="#">Test</a></li>';
 
     $navbar .= '</ul>';
 
@@ -37,13 +37,10 @@ function navbar_login($logged_in){
         $return_string = '';
     }else{
         $return_string = '<li>
-                              <button class="btn btn-secondary dropdown-toggle d-none d-lg-block" type="button" id="login_menu" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <i class="fas fa-sign-in-alt"></i>
-                              </button>
-                              <button class="btn btn-secondary btn-block dropdown-toggle d-lg-none" type="button" id="login_menu" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                Login
-                              </button>
-                              <div class="dropdown-menu dropdown-menu-right" aria-labelledby="login_menu">
+                              <a class="nav-link dropdown-toggle" href="#" id="navbar_login" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Log In
+                              </a>
+                              <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbar_login">
                                     <form class="px-3 py-2">
                                         <div class="form-group">
                                             <label for="login_email">Email Address</label>
@@ -54,7 +51,7 @@ function navbar_login($logged_in){
                                             <input type="password" class="form-control" id="login_password" placeholder="Password">
                                             <div class="help-block text-right small"><a href="">Forget your password?</a></div>
                                         </div>
-                                        <button type="submit" class="btn btn-success btn-block">Login</button>                   
+                                        <button type="submit" class="btn btn-success btn-block">Log In</button>                   
                                         <div class="form-group form-check">
                                             <input type="checkbox" class="form-check-input" id="remember_check">
                                             <label class="form-check-label" for="remember_check">Remember Me</label>
